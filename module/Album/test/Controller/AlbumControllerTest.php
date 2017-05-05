7d02f2c8c7bf55e44227ab502040d1bc51bc1b89
Sébastien Serre <sserre.bx@gmail.com>
2017-05-05 12:22:31 +0200

    add tests

---
+++ module/Album/test/Controller/AlbumControllerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AlbumControllerTest
+ *
+ * @author Sébastien Serre <sserre at msha.fr>
+ */
+
+namespace AlbumTest\Controller;
+
+use Album\Controller\AlbumController;
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+
+class AlbumControllerTest extends AbstractHttpControllerTestCase {
+
+    protected $traceError = true;
+
+    public function setUp() {
+        // The module configuration should still be applicable for tests.
+        // You can override configuration here with test case specific values,
+        // such as sample view templates, path stacks, module_listener_options,
+        // etc.
+        $configOverrides = [];
+
+        $this->setApplicationConfig(ArrayUtils::merge(
+                // Grabbing the full application configuration:
+                include __DIR__ . '/../../../../config/application.config.php', $configOverrides
+        ));
+        parent::setUp();
+        $services = $this->getApplicationServiceLocator();
+        $config = $services->get('config');
+        unset($config['db']);
+        $services->setAllowOverride(true);
+        $services->setService('config', $config);
+        $services->setAllowOverride(false);
+    }
+
+    public function testIndexActionCanBeAccessed() {
+        $this->dispatch('/albums');
+        $this->assertResponseStatusCode(200);
+        $this->assertModuleName('Album');
+        $this->assertControllerName(AlbumController::class);
+        $this->assertControllerClass('AlbumController');
+        $this->assertMatchedRouteName('album');
+    }
+
+}
