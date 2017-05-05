dcd7ce1311db82567c52356d010e533babe46822
Sébastien Serre <sserre.bx@gmail.com>
2017-05-05 14:07:53 +0200

    sql create table missing

--- data/develdata.sql
+++ data/develdata.sql
@@ -1,3 +1,4 @@
+CREATE TABLE album (id INTEGER PRIMARY KEY AUTOINCREMENT, artist varchar(100) NOT NULL, title varchar(100) NOT NULL);
 INSERT INTO "album" ("artist", "title")
 VALUES
     ("David Bowie", "The Next Day (Deluxe Version)"),
